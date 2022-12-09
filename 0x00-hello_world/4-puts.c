2998167da0c281f115e784d460c25dfc51fb507c
Ezeh Christian Chiedozie <edozievip898@gmail.com>
2022-12-08 21:01:02 -0800

    good

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,8 @@
 #!/bin/bash
  #include <stdio.h>                                                                                                                      
-/**                                                                                                                                     
- *  * main - Entry                                                                                                                         
- *   * Return: Always 0                                                                                                                     
- *    */                                                                                                                                     
+/**                                                                             * main - Entry                                                      
+* Return: Always 0                                                              */                                                                                                                                     
 int main(void)                                                                                                                          
-{                                                                                                                                       
-	        printf("with proper grammar, but the outcome is a piece of art,\n");                                                            
-		        return (0);)
+{                                                
+	printf("with proper grammar, but the outcome is a piece of art,\n");            return (0);)
 }
