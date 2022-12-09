4806c274909c3778103e8d1eeb8054e88d5a52d6
Ezeh Christian Chiedozie <edozievip898@gmail.com>
2022-12-09 09:31:09 -0800

    compile all

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+* main - Entry
+* Return: Always 0
+*/
+int main(void)
+{
+	write("and that piece of art is useful\" - Dora Korpa, 2015-10-19");
+	return (1);
+}
