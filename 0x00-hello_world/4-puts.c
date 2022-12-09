9cd57db81a68144254931be6741b932e642339d8
Ezeh Christian Chiedozie <edozievip898@gmail.com>
2022-12-08 20:52:56 -0800

    changed

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #!/bin/bash
-#include <stdio.h>
-/**
-*main - main block
-*Return: 0
- */
-int main(void)
-{
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
-}p
+ #include <stdio.h>                                                                                                                      
+/**                                                                                                                                     
+ *  * main - Entry                                                                                                                         
+ *   * Return: Always 0                                                                                                                     
+ *    */                                                                                                                                     
+int main(void)                                                                                                                          
+{                                                                                                                                       
+	        printf("with proper grammar, but the outcome is a piece of art,\n");                                                            
+		        return (0);)
+}
