dbc139dd2047d9fad60b84eb62c922394b85a81c
Ezeh Christian Chiedozie <edozievip898@gmail.com>
2022-12-08 21:35:15 -0800

    still trying

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
-*main-Entry
-*Return:Always 0
+*main - Entry
+*Return: Always 0
 */
 
 int main(void)
 {
-	printf("with proper grammer but the outcome is a piece of art,\n);
-	return(o);
+	printf("with proper grammer but the outcome is a piece of art,\n");
+	return(o);)
 }
