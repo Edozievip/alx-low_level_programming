524f16a433eee2f03ce1a7cdd0969e86801e4bee
Ezeh Christian Chiedozie <edozievip898@gmail.com>
2022-12-10 14:36:19 -0800

    if loop

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,19 +13,19 @@ int main(void)
 	int n;
 
 	srand(time(0));
-	n = rand() - RAND_MAX/2;
+	n = rand() - RAND_MAX / 2;
 
-	if (n > 0)
+	if (n < 0)
 	{
-		printf("%i is positive\n", n);
+		printf("%d is negative\n", n);
 	}
 	else if (n == 0)
 	{
-		printf("%i is zero\n", n);
+		printf("%d is zero\n", n);
 	}
-	else if (n < 0)
+	else
 	{
-		printf("%i is negative\n", n);
+		printf("%d is positive\n", n);
 	}
 	return (0);
 }
