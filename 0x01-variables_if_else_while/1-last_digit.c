3af5c5b28ef4981711120c456cdf111dbf6f8bd7
Ezeh Christian Chiedozie <edozievip898@gmail.com>
2022-12-09 20:51:44 -0800

    good

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,9 +14,9 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX/2;
 
-	printf("Last digit of %i and is ", n, (n % 10));
+	printf("Last digit of %i is %i and is ", n, (n % 10));
 
-	if ((n % 10) == 0);
+	if ((n % 10) == 0)
 	{
 		printf("0\n");
 	}
