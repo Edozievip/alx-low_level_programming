c265fd01b972e1dcde0b1d56256d86d4aa7ceb3a
Ezeh Christian Chiedozie <edozievip898@gmail.com>
2022-12-11 08:55:47 -0800

    we combine the world through coding

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -19,7 +19,7 @@ int main(void)
 				putchar(i % 10 + '0');
 				putchar(' ');
 				putchar(j / 10 + '0');
-				putchar(j % 10 + '0')
+				putchar(j % 10 + '0');
 					if (i * 100 + j != 9899)
 					{
 						putchar(',');
