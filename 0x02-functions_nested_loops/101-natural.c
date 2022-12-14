66e8b38df5430076838e109d446d173750f6d792
Ezeh Christian Chiedozie <edozievip898@gmail.com>
2022-12-14 15:10:54 -0800

    write the sum of all multiples of 3 and 5

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - sum of multiplied numbers
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int i, z = 0;
+
+	while (i < 1024)
+	{
+	if ((i % 3 == 0) || (i % 5 == 0))
+	{
+	z += i;
+	}
+	i++;
+	}
+	printf("%d\n", z);
+	return (0);
+}
