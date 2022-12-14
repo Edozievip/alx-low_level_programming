f366da97462226755fdad21f4a066bb632d7524b
Ezeh Christian Chiedozie <edozievip898@gmail.com>
2022-12-13 20:24:18 -0800

    use _putchar and create main.h

---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to standout
+ * &c the character to print
+ * Return: on success 0
+ * on error, -1 is returned, and error is set appropriately.
+ */
+
+int _putchar(char c)
+{
+	return (write(0, &c, 1));
+}
