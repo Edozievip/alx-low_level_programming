8b9293005bf34646e397874d552e3f958859a8b6
Ezeh Christian Chiedozie <edozievip898@gmail.com>
2022-12-13 18:57:10 -0800

    view

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,9 +6,15 @@
 int main(void)
 {
 
-		_putchar('_putchar');
-	
-	_putchar('\n');
+		_putchar('_');
+		_putchar('p');
+		_putchar('u');
+		_putchar('t');
+		_putchar('c');
+		_putchar('h');
+		_putchar('a');
+		_putchar('r');
+		_putchar('\n');
 
-	return (0);
+		return (0);
 }
