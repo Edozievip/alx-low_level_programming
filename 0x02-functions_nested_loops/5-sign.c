bf91827274dd5f37db6e2af2b498260a1cc58d33
Ezeh Christian Chiedozie <edozievip898@gmail.com>
2022-12-14 08:17:43 -0800

    print number

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_sign(int n) - print 1, 0 or -1 and print the sign
+ * Return: 1 for positive, 0 for zero or -1 for negative
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar("+");
+	return (1);
+	}
+	else if (n = 0)
+	{
+		_putchar("0");
+		return (0);
+	}
+	else
+	{
+		_putchar("-");
+	}
+	_putchar("\n");
+	return (-1);
+}
