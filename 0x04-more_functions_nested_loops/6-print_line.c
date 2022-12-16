7b812b565c9257a07852a49e93f22a734f9883e2
Ezeh Christian Chiedozie <edozievip898@gmail.com>
2022-12-16 03:31:03 -0800

    draw a line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_line - Draw a staight line in the termonL
+ * Return: empty
+ * @n: the number of lines to draw
+ */
+
+void print_lines(int n)
+{
+	int x;
+
+	if (n <= 0)
+	{
+	_putchar('\n');
+	}
+	else
+	{
+		for (x = 0; x < n; x++)
+		{
+		_putchar(95);
+		}
+		_putchar('\n');
+	}
+}
