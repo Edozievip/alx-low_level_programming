665d23e6f69a66292e47b80156c8dc6ea2314847
Ezeh Christian Chiedozie <edozievip898@gmail.com>
2022-12-15 19:32:13 -0800

    check for uppercase

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
-/** _isupper - check if a letter is upper
- * @c: the number to be checked
+/**
+ * _isupper - check if a letter is upper
+ * @x: the number to be checked
  * Return: 1 for uppercase and 0 for otherwise
  */
 
