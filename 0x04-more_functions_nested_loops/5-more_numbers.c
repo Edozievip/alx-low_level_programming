af553bd69f0145a815f0f9690ea216a00c02031c
Ezeh Christian Chiedozie <edozievip898@gmail.com>
2022-12-16 03:12:49 -0800

    print 10 times from 0 to 14

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * more_numbers - print ten times  from 0 to 1r
+ * Return: the multiplied values
+ */
+
+void more_numbers(void)
+
+{
+	int x, y;
+
+	for (x = 0; x < 10; x++)
+	{
+	for (y = 0; y <= 14; y++)
+	{
+	if (y > 9)
+	{
+	_putchar((y / 10) + '0');
+	}
+	_putchar((y % 10) + '0');
+	}
+	_putchar('\n');
+	}
+}
