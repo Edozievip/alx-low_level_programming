24225a55b61a4befbd7a63d898f442f8626efd5f
Ezeh Christian Chiedozie <edozievip898@gmail.com>
2022-12-17 03:33:06 -0800

    print alpha and num

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,17 +12,17 @@ int main(void)
 
 	for (a = 1; a < 100; a++)
 	{
-	if (a % 3 == 0)
+	if ((a % 3 == 0) && (a % 5 == 0))
+	{
+		printf("FizzBuzz");
+	}
+	else if (a % 3 == 0)
 	{
 		printf("Fizz");
 	}
 	else if (a % 5 == 0)
 	{
 		printf("Buzz");
-	}
-	else if ((a % 5 == 0) && (a % 3 == 0))
-	{
-		printf("FizzBuzz");
 	}
 	else
 	{
@@ -33,6 +33,6 @@ int main(void)
 		printf(" ");
 	}
 	}
-	printf('\n');
+	printf("\n");
 	return (0);
 }
