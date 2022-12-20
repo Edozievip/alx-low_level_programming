ed90e81d5753a9ef9e2dfacc621e912a55b41d20
Ezeh Christian Chiedozie <edozievip898@gmail.com>
2022-12-19 19:07:50 -0800

    return the lenght of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+nclude "main.h"
+
+/**
+* _strlen - returns the length of a string
+* @s: string
+* Return: lengtht
+*/
+
+int _strlen(char *s)
+{
+	int longi = 0;
+
+	while (*s != '\0')
+	{
+	longi++;
+	s++;
+	}
+	return (longi);
+}
+
