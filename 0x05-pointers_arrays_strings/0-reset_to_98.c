bb0965c1b7f4ae61bcb14688a6e4cdd011b41585
Ezeh Christian Chiedozie <edozievip898@gmail.com>
2022-12-19 18:45:07 -0800

    reset the values

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,15 +2,11 @@
 
 /**
  * reset_to_98 - takes a pointer to an int as parameter and updates the va
+ * @n: the pointing parameter 
  * Return: Always 0
  */
 
 void reset_to_98(int *n)
 {
-	*n = 402;
-
-	_putchar("%d\n", n);
-	n = 98;
-	_putchar("%d\n", n);
-	return (0);
+	*n = 98;
 }
