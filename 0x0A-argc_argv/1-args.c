57f55f25bb733f1e2d8f45a1c49ab5dc94c3bb73
Ezeh Christian Chiedozie <edozievip898@gmail.com>
2023-01-07 19:37:14 -0800

    firdt commit

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/*
+ * main - Prints the number of args
+ * @argc: arguement count
+ * @argv: arguement vector
+ * Return: Always zero
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argv;
+	printf("%d\n", argc - 1);
+		return (0);
+}
