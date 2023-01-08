e20de5922f95f6ebc518ca89d8c940ea758bf6b4
Ezeh Christian Chiedozie <edozievip898@gmail.com>
2023-01-07 18:55:35 -0800

    first commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio>
+
+/*
+ * main - print the name
+ * @argc: argiement count
+ * @argv: arguement vector
+ * Return: always 0
+ */
+
+int main (int argc, char *argv[])
+{
+	(void) arg;
+	printf("%s\n", argv[0]);
+	return (0);
+}
