31baa8825c1e1083a2502c34464f0277696f698c
Ezeh Christian Chiedozie <edozievip898@gmail.com>
2023-01-17 11:26:49 -0800

    final commit

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,21 +1,19 @@
 #ifndef DOG_H
 #define DOG_H
-
 /**
  * struct dog - user defined data type
  * @name: name of the dog
- * @age: the age of the dog
- * @owner: the dog owner
+ * @age: age of the dog
+ * @owner: owner of the dog
+ * author: Edozievip
  */
-
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}
+};
 typedef struct dog dog_t;
-
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
