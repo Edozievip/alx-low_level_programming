a0c705645c3e553c729cf84053715b639de84557
Ezeh Christian Chiedozie <edozievip898@gmail.com>
2023-01-19 08:53:21 -0800

    first commit

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -19,17 +19,17 @@ int main(int argc, char *argv[])
 		exit(1);
 	}
 	nbytes = atoi(argv[1]);
-	    if (nbytes < 0)
-	    {
-		    printf("Error\n");
-		    exit(2);
-	    }
-	    for (i = 0; i < nbytes; i++)
-	    {
-		    printf("%02x", opc[i] & 0xFF);
-		    if (i != nbytes - 1)
-			    printf(" ");
-	    }
-	    printf("\n");
-	    return (0);
+	if (nbytes < 0)
+	{
+	printf("Error\n");
+	exit(2);
+	}
+	for (i = 0; i < nbytes; i++)
+	{
+	printf("%02x", opc[i] & 0xFF);
+	if (i != nbytes - 1)
+	printf(" ");
+	}
+	printf("\n");
+	return (0);
 }
