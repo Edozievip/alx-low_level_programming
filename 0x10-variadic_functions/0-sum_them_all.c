087d1864a49cd3f842337dfed1791a298896ca2c
Ezeh Christian Chiedozie <edozievip898@gmail.com>
2023-01-19 20:53:14 -0800

    co.mit all

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,23 +1,22 @@
-#include "variadic_fuctions.h"
+#include "variadic_functions.h"
 
 /**
- * sum_them_all - sum all given parameters
- * @n: param
- * Return: int
- */
+  * sum_them_all - adds num
+  * @n: param
+  * Return: int
+  */
 
 int sum_them_all(const unsigned int n, ...)
 {
-	va_list list;
+	va_list valist;
 	unsigned int i;
 	int sum = 0;
 
 	if (n == 0)
-	return (0);
-
-	va_start(list, n);
+		return (0);
+	va_start(valist, n);
 	for (i = 0; i < n; i++)
-		sum += va_arg(list, int);
-	va_end(list);
+		sum += va_arg(valist, int);
+	va_end(valist);
 	return (sum);
 }
