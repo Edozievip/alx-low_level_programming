793f659ec3794b3d12d1cdafb6bef4c8a068bb76
Ezeh Christian Chiedozie <edozievip898@gmail.com>
2023-07-12 15:40:17 +0100

    commit all

---
+++ 0x1E-search_algorithms/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "search_algos.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always EXIT_SUCCESS
+ */
+int main(void)
+{
+    int array[] = {
+        10, 1, 42, 3, 4, 42, 6, 7, -1, 9
+    };
+    size_t size = sizeof(array) / sizeof(array[0]);
+
+    printf("Found %d at index: %d\n\n", 3, linear_search(array, size, 3));
+    printf("Found %d at index: %d\n\n", 9, linear_search(array, size, 9));
+    printf("Found %d at index: %d\n", 99, linear_search(array, size, 99));
+    return (EXIT_SUCCESS);
+}
